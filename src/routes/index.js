b1e6517b0af7b1a0587bdeb798c6375ac7fbd85f
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-08-18 23:54:19 -0500

    Formulario terminado
    Funcionando correctamente con gmail, solo se agrego contraseña de aplicacion en gmail.

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,21 +6,21 @@ router.post('/send-email', async (req, res) => {
     const {name, email, phone, message} = req.body;
 
     contentHTML = `
-        <h1>Cliente</h1>
+        <p>Cliente</p>
         <ul>
-            <li>Nombre: ${name} </li>
-            <li>Correo electronico: ${email} </li>
-            <li>Telefono: ${phone} </li>
+            <li> <p>Nombre: ${name}</p> </li>
+            <li><p>Correo electronico: ${email}</p> </li>
+            <li><p>Telefono: ${phone}</p></li>
         </ul>
         <p>${message}</p>
     `;
     
     const transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+        host: "smtp.gmail.com",
+        port: 587,
         auth: {
-          user: "403a477d3934dc",
-          pass: "6e1df527a6028c"
+          user: "marcocaos11@gmail.com",
+          pass: "fmcsiuhmpndzhfyn"
         },
         tls:{
             rejectUnauthorized: false
@@ -30,12 +30,18 @@ router.post('/send-email', async (req, res) => {
     const info = await transporter.sendMail({
         from: "Consultorio",
         to: "marcocarrilloosuna@gmail.com",
-        subject: "Cliente😎",
+        subject: "🎉Cliente Nuevo🎉",
         html: contentHTML
     });
 
-    console.log("Mensaje enviado", info.messageId);
-    res.redirect('/success.html');
+    if(info.messageId){
+        console.log("Mensaje enviado", info.messageId);
+        res.redirect('/success.html');
+    }else{
+        console.log('Mensaje fallido');
+        res.redirect('/')
+    }
+    
     
 });
 
