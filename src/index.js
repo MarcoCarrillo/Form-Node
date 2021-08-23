62b621b409ead8adaa5f41d3f23076ae3a1d26ac
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-08-23 00:56:51 -0500

    arreglo de comando

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//Habilitar CORS
-app.use(cors({ credentials: true, origin: true }));
-app.options("*", cors());
+
 
 //Habilitar express.json para las solicitudes de POST y reconoce que es .json el objeto del request, se pueden hacer los requests con content-type: application.json desde headers en postman
 app.use(express.json({extends: true}));
 
-//Si hay puerto en variable de entorno tomar ese si no el puerto 4000
-const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({extended: false})); //Entender los datos del form
 
@@ -18,10 +14,10 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, 'public'))); //Carpeta publica del proyecto
 
-//Si hay puerto en variable de entorno tomar ese si no el puerto 4000
-const port = process.env.PORT || 4000;
+//  use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const puerto = process.env.PORT || 3000;
 
-//Arrancar el servidor
-app.listen(port, '0.0.0.0', () =>{
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
-})
+app.listen(puerto, host, function() {
+    console.log("Server started.......", puerto);
+  });
